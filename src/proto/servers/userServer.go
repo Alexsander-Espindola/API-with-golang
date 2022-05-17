cebed05354041081d25ada80f1b85db767ce6b2b
Alexsander Espindola <alexsander81002745@gmail.com>
2022-05-17 17:45:03 -0300

    Criando server para o user

--- src/proto/servers/userServer.go
+++ src/proto/servers/userServer.go
@@ -3,11 +3,29 @@ package servers
 import (
 	"context"
 
+	"github.com/Alexsander-Espindola/API-with-golang/src/model"
 	"github.com/Alexsander-Espindola/API-with-golang/src/proto/pb"
 )
 
-type postUserClient struct{}
+type PostUserClient struct {
+	pb.UnimplementedPostUserServer
+}
 
-func (c *postUserClient) PostUser(ctx context.Context, in *pb.User) (*pb.Response, error) {
+func (c *PostUserClient) PostUser(ctx context.Context, in *pb.User) (*pb.UserResponse, error) {
+	endereco := model.Endereco{
+		Cidade: in.GetCidade(),
+		Estado: in.GetEstado(),
+	}
 
+	user := model.User{
+		Name:     in.GetName(),
+		Email:    in.GetEmail(),
+		Endereco: endereco,
+	}
+
+	model.PostUser(user)
+
+	return &pb.UserResponse{
+		Status: 201,
+	}, nil
 }
